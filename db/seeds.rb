80f894b63730217edf63babd4280978f3de7ad27
as.ja22 <as_ja22@yahoo.com>
2018-12-06 18:56:25 +0800

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 require 'csv'
 
+puts "Importing departmet..."
+CSV.foreach(Rails.root.join("department.csv"), headers: true) do |row|
+  Department.create! do |deprtment|
+    department.name = row[0]
+  end
+end
+
 puts "Importing position..."
 CSV.foreach(Rails.root.join("positions.csv"), headers: true) do |row|
   Position.create! do |position|
